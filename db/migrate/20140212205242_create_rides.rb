bb17e4fb0836d7c94fa03caae86ec995c7b3217b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-23 00:58:23 +0000

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,9 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.references :passenger, index: true, foreign_key: true
+      t.references :taxi, index: true, foreign_key: true
+
       t.timestamps null: false
     end
   end
