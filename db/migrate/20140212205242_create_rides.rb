aa625706af41761e90c6819cacb5d9c9d3668d24
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-23 00:56:39 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,8 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-      t.integer :passenger_id, foreign_key: true
-      t.integer :taxi_id, foreign_key: true
+      t.references :passenger, index: true, foreign_key: true
+      t.references :taxi, index: true, foreign_key: true
 
       t.timestamps null: false
     end
